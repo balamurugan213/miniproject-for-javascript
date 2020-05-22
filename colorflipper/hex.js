c3e69d7cf6f490f7862ef26fc1a27a5e82c1f830
balamurugan213 <balainfo21@gmail.com>
2020-05-22 14:42:29 -0700

    push 1

---
+++ colorflipper/hex.js
@@ -0,0 +1,39 @@
+const hex = [
+	0,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F'
+]
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+	// get random num
+
+	let hexcolor = '#'
+	for (let i = 0; i < 6; i++) {
+		hexcolor += hex[getRandomNum()]
+	}
+
+	// const randomNumber = getRandomNum()
+	// console.log(randomNumber)
+	document.body.style.backgroundColor = hexcolor
+	color.textContent = hexcolor
+})
+
+function getRandomNum(){
+	return Math.floor(Math.random() * hex.length)
+}
